e0bc26fcfebedef6f6eea9cbd1beeaeba87cd78b
ali <syedaliibrahim50@gmail.com>
2022-09-06 14:41:32 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let mainObj={
+    id:0,
+    obj1:{ id:1,
+        obj2:{ id:2,
+            obj3:{ id:3,
+                obj4:{
+                    id:4,
+                }
+            }
+        }    
+    }
+}
+let layer4={...mainObj,obj1,obj2,obj3,obj4}
+console.log(layer4)
